7f36b539d97a40d296c680286aeedefa653608b1
romasmi <rsmarsu@gmail.com>
2026-03-10 21:53:07 +0300

    HW4: Add users migration.

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id uuid PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    updated_at TIMESTAMP DEFAULT NOW(),
+    created_at TIMESTAMP DEFAULT NOW()
+);
